79a1b822f1421fd4ceed809bcf728692a5d800e4
Carlijn van de Weijer <carlijn.vandeweijer@ontwerpwerk.nl>
2021-03-09 22:49:53 +0100

    📄  Add individual Article page(s)

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import Html from "../components/html";
 
 export default function ArticlePage() {
   const params = useParams();
+  const [article, setArticle] = useState("");
+
+  useEffect(() => {
+    async function getArticle() {
+      const response = await axios.get(
+        `http://traveldestinations.dd:8083/jsonapi/node/article/${params.articleId}`
+      );
+      console.log("what is response ", response.data.data);
+      setArticle(response.data.data);
+    }
+    getArticle();
+  }, [params.articleId]);
+
+  const { title, body, changed } = article.attributes || {};
 
   return (
     <div className="article">
-      <h1>hi i'm articlepage</h1>
+      <h1>{title}</h1>
+      <Html>{body?.processed}</Html>
     </div>
   );
 }
