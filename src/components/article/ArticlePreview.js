d943e60466a4466b43a5e72bc8fa84b2bbdd7a07
Carlijn van de Weijer <carlijn.vandeweijer@ontwerpwerk.nl>
2021-03-09 23:48:09 +0100

    💄  Update preview styling

--- src/components/article/ArticlePreview.js
+++ src/components/article/ArticlePreview.js
@@ -4,7 +4,7 @@ import Html from "../html";
 
 export default function ArticlePreview({ id, title, description }) {
   return (
-    <div className="article__preview">
+    <div className="blog__preview">
       <Link to={`/blog/${id}`}>
         <h2>{title}</h2>
       </Link>
